43d1d6a00ac61a9419a9e0316807db6d767f3a6d
Juan Agustin Zitelli <juanzitelli7@gmail.com>
2023-09-21 16:15:11 -0300

    update characters and starships resolvers

--- src/characters/characters.resolver.spec.ts
+++ src/characters/characters.resolver.spec.ts
@@ -1,28 +1,58 @@
+import { BadRequestException } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
 import { PrismaService } from "./../db/prisma.service";
 import { PlanetsService } from "./../planets/planets.service";
+import { StarshipsResolver } from "./../starships/starships.resolver";
 import { StarshipsService } from "./../starships/starships.service";
 import { CharactersResolver } from "./characters.resolver";
 import { CharactersService } from "./characters.service";
+import { CreateCharacterInput } from "./dto/create-character.input";
 import { EmbarkCharacterInput } from "./dto/embark-character.input";
 
 describe("CharactersResolver", () => {
   let resolver: CharactersResolver;
-  let characterService: CharactersService;
+  let charactersService: jest.Mocked<CharactersService>;
+  let planetsService: jest.Mocked<PlanetsService>;
+  let starshipsService: jest.Mocked<StarshipsService>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef: TestingModule = await Test.createTestingModule({
       providers: [
+        StarshipsResolver,
         CharactersResolver,
-        CharactersService,
-        StarshipsService,
-        PlanetsService,
         PrismaService,
+        {
+          provide: CharactersService,
+          useValue: {
+            create: jest.fn(),
+          },
+        },
+        {
+          provide: StarshipsService,
+          useValue: {
+            create: jest.fn(),
+            findOne: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+        {
+          provide: PlanetsService,
+          useValue: {
+            findOne: jest.fn(),
+          },
+        },
       ],
     }).compile();
-
-    resolver = module.get<CharactersResolver>(CharactersResolver);
-    characterService = module.get<CharactersService>(CharactersService);
+    charactersService = moduleRef.get<CharactersService>(
+      CharactersService,
+    ) as unknown as jest.Mocked<CharactersService>;
+    resolver = moduleRef.get<CharactersResolver>(CharactersResolver);
+    planetsService = moduleRef.get<PlanetsService>(
+      PlanetsService,
+    ) as jest.Mocked<PlanetsService>;
+    starshipsService = moduleRef.get<StarshipsService>(
+      StarshipsService,
+    ) as jest.Mocked<StarshipsService>;
   });
 
   it("should be defined", () => {
@@ -35,17 +65,63 @@ describe("CharactersResolver", () => {
       starshipId: 1,
     };
 
-    characterService.update = jest.fn().mockResolvedValue({
+    charactersService.update = jest.fn().mockResolvedValue({
       id: params.characterId,
     });
 
-    characterService.findOne = jest.fn().mockResolvedValue({
+    starshipsService.findOne = jest.fn().mockResolvedValue({
       cargoCapacity: 0,
       name: "ship",
     });
 
     await expect(async () =>
-      characterService.embark(params),
+      charactersService.embark(params),
     ).rejects.toThrowError();
+  });
+
+  it("should create a new character when provided valid data", async () => {
+    const params: CreateCharacterInput = {
+      name: "Han Solo",
+      sensitivityToTheForce: "Low",
+      species: "Human",
+      currentLocationId: 13,
+    };
+
+    planetsService.findOne.mockResolvedValue({
+      id: params.currentLocationId,
+      name: "Jupiter",
+      climate: "Gas Giant",
+      latitude: 64.52151816723162,
+      longitude: -63.95491918219119,
+      population: 0,
+      terrain: "Gas",
+    });
+
+    const expectedValue = {
+      id: 1,
+      starshipId: null,
+      ...params,
+    };
+
+    charactersService.create.mockResolvedValue(expectedValue);
+
+    expect(await resolver.createCharacter(params)).toEqual(expectedValue);
+  });
+
+  it("should throw an error when provided an invalid planet id", async () => {
+    const params: CreateCharacterInput = {
+      name: "new character",
+      currentLocationId: 1,
+      sensitivityToTheForce: "Low",
+      species: "Human",
+    };
+
+    planetsService.findOne.mockResolvedValue(null);
+
+    await expect(resolver.createCharacter(params)).rejects.toThrow(
+      new BadRequestException(
+        `Couldn't find a planet with the given currentLocationId`,
+      ),
+    );
   });
 });
--- src/starships/starships.resolver.spec.ts
+++ src/starships/starships.resolver.spec.ts
@@ -1,26 +1,138 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { PrismaService } from "./../db/prisma.service";
 import { PlanetsService } from "./../planets/planets.service";
+import { CreateStarshipInput } from "./dto/create-starship.input";
 import { StarshipsResolver } from "./starships.resolver";
 import { StarshipsService } from "./starships.service";
 
 describe("StarshipsResolver", () => {
   let resolver: StarshipsResolver;
+  let planetsService: jest.Mocked<PlanetsService>;
+  let starshipsService: jest.Mocked<StarshipsService>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef: TestingModule = await Test.createTestingModule({
       providers: [
         StarshipsResolver,
-        StarshipsService,
-        PlanetsService,
+
         PrismaService,
+        {
+          provide: StarshipsService,
+          useValue: {
+            create: jest.fn(),
+            findOne: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+        {
+          provide: PlanetsService,
+          useValue: {
+            findOne: jest.fn(),
+          },
+        },
       ],
     }).compile();
-
-    resolver = module.get<StarshipsResolver>(StarshipsResolver);
+    resolver = moduleRef.get<StarshipsResolver>(StarshipsResolver);
+    planetsService = moduleRef.get<PlanetsService>(
+      PlanetsService,
+    ) as unknown as jest.Mocked<PlanetsService>;
+    starshipsService = moduleRef.get<StarshipsService>(
+      StarshipsService,
+    ) as unknown as jest.Mocked<StarshipsService>;
   });
 
   it("should be defined", () => {
     expect(resolver).toBeDefined();
+  });
+
+  it("should create a starship", async () => {
+    const createStarshipInput: CreateStarshipInput = {
+      cargoCapacity: 1000,
+      currentPlanetId: 12,
+      latitude: 22.0,
+      longitude: 23.0,
+      model: "A-Wing",
+      name: "Star Destroyer",
+    };
+
+    planetsService.findOne.mockResolvedValueOnce({
+      id: createStarshipInput.currentPlanetId,
+      name: "Jupiter",
+      climate: "Gas Giant",
+      latitude: 64.52151816723162,
+      longitude: -63.95491918219119,
+      population: 0,
+      terrain: "Gas",
+    });
+
+    starshipsService.create.mockResolvedValueOnce({
+      id: 1,
+      cargoCapacity: 1000,
+      currentPlanetId: 12,
+      latitude: 22.0,
+      longitude: 23.0,
+      model: "A-Wing",
+      name: "Star Destroyer",
+      starshipId: 1,
+    });
+
+    const result = await resolver.createStarship(createStarshipInput);
+    expect(result).toBeDefined();
+    expect(starshipsService.create).toHaveBeenCalledWith({
+      data: createStarshipInput,
+    });
+  });
+
+  it("should throw an error when no planet is found during starship creation", async () => {
+    const createStarshipInput: CreateStarshipInput = {
+      cargoCapacity: 1000,
+      currentPlanetId: 12,
+      latitude: 22.0,
+      longitude: 23.0,
+      model: "A-Wing",
+      name: "Star Destroyer",
+    };
+    planetsService.findOne.mockResolvedValueOnce(null);
+
+    await expect(
+      resolver.createStarship(createStarshipInput),
+    ).rejects.toThrowError();
+  });
+
+  it("should remove a starship", async () => {
+    const id: number = 1;
+
+    starshipsService.findOne.mockResolvedValueOnce({
+      id,
+      cargoCapacity: 1000,
+      currentPlanetId: 12,
+      latitude: 22.0,
+      longitude: 23.0,
+      model: "A-Wing",
+      name: "Star Destroyer",
+      starshipId: 1,
+    });
+
+    starshipsService.remove.mockResolvedValueOnce({
+      id: 1,
+      cargoCapacity: 1000,
+      currentPlanetId: 12,
+      latitude: 22.0,
+      longitude: 23.0,
+      model: "A-Wing",
+      name: "Star Destroyer",
+      starshipId: 1,
+    });
+
+    const result = await resolver.removeStarship(id);
+    expect(result).toBeDefined();
+    expect(starshipsService.remove).toHaveBeenCalledWith({ where: { id } });
+  });
+
+  it("should throw an error when trying to remove a non-existent starship", async () => {
+    const id: number = 1;
+    starshipsService.findOne.mockResolvedValueOnce(null);
+
+    await expect(resolver.removeStarship(id)).rejects.toThrowError();
   });
 });
