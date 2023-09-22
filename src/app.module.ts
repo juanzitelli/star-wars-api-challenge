1dc7ed0af48c805004da1e08be67214a3bc6daee
Juan Agustin Zitelli <juanzitelli7@gmail.com>
2023-09-22 12:51:46 -0300

    add stack trace and fix error messages

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { AppService } from "./app.service";
 import { CharactersModule } from "./characters/characters.module";
 import { PlanetsModule } from "./planets/planets.module";
 import { StarshipsModule } from "./starships/starships.module";
+import { GraphQLFormattedError } from "graphql";
 
 @Module({
   imports: [
@@ -16,6 +17,13 @@ import { StarshipsModule } from "./starships/starships.module";
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), "src/schema.gql"),
       sortSchema: true,
+      formatError: (formattedError: GraphQLFormattedError) => {
+        return {
+          message: formattedError.extensions.originalError?.["message"],
+          status: formattedError.extensions.originalError?.["statusCode"],
+          error: formattedError.extensions.originalError?.["error"],
+        };
+      },
     }),
     CharactersModule,
     PlanetsModule,
